ef1e451b4c3abe795ca65ef69a91be2f3e01e76f
BATMANINDIAN12 <125759883+BATMANINDIAN12@users.noreply.github.com>
2025-12-26 19:53:53 -0600

    Reduce feeder motor power in teleop drive
    Adjusted the feeder motor power from ±0.5 to ±0.1 for both forward and backward directions in RedRobotTeleopMecanumFieldRelativeDrive. This change likely aims to provide finer control or prevent excessive speed during teleop operation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRobotTeleopMecanumFieldRelativeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRobotTeleopMecanumFieldRelativeDrive.java
@@ -239,10 +239,10 @@ public class RedRobotTeleopMecanumFieldRelativeDrive extends OpMode {
 
         if (feederState == 1) {
             // Forward
-            robot.feedingRotation.setPower(-0.5);
+            robot.feedingRotation.setPower(-0.1);
         } else if (feederState == -1) {
             // Backward
-            robot.feedingRotation.setPower(0.5);
+            robot.feedingRotation.setPower(0.1);
         } else {
             // Off
             robot.feedingRotation.setPower(0);
