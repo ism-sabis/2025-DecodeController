20bf2beeb44743d62d699f2e084e9ed6e292f246
BATMANINDIAN12 <125759883+BATMANINDIAN12@users.noreply.github.com>
2026-01-12 16:06:34 -0600

    Add AnalogInput import to auto drive opmode
    Imported com.qualcomm.robotcore.hardware.AnalogInput in RedAudienceRobotAutoDriveByEncoder_Linear.java, likely in preparation for using analog sensors in the autonomous routine.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAudienceRobotAutoDriveByEncoder_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAudienceRobotAutoDriveByEncoder_Linear.java
@@ -37,6 +37,7 @@ import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
