7b34e9e2a19cb0aa3fec951e5e849da692fc1233
BATMANINDIAN12 <125759883+BATMANINDIAN12@users.noreply.github.com>
2026-02-25 17:01:57 -0600

    start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alexLearn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alexLearn.java
@@ -61,7 +61,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Robot: Auto Drive By Encoder", group="Robot")
+@Autonomous(name="alexLearn", group="Robot")
 @Disabled
 public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
 
